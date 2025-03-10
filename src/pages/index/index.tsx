e2a147865c7071ed82ae3762b07dda7ca1902d69
xogus3492 <wkdxogus3492@gmail.com>
2025-03-10 23:01:30 +0900

    feat: SearchBar 컴포넌트 구현

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,4 +1,5 @@
 import CommonHeader from '@/components/common/header/CommonHeader'
+import CommonSearchBar from '@/components/common/searchBar/CommonSearchBar'
 import styles from './styles/index.module.scss'
 
 function index() {
@@ -16,6 +17,7 @@ function index() {
                             모든 지역에 있는 크리에이터들의 지원을 받습니다.
                         </span>
                         {/* 검색창 UI 부분 */}
+                        <CommonSearchBar />
                     </div>
                 </div>
                 <div className={styles.page__contents__imageBox}></div>
