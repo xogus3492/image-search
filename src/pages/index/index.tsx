c069c34168d565d8336522f516cb76a3a297e541
xogus3492 <wkdxogus3492@gmail.com>
2025-03-10 17:18:19 +0900

    feat: Header 컴포넌트 구현

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,9 +1,11 @@
+import CommonHeader from '@/components/common/header/CommonHeader'
 import styles from './styles/index.module.scss'
 
 function index() {
     return (
         <div className={styles.page}>
             {/* 공통 헤더 UI 부분 */}
+            <CommonHeader />
             {/* 공통 네비게이션 UI 부분 */}
             <div className={styles.page__contents}>
                 <div className={styles.page__contents__introBox}>
