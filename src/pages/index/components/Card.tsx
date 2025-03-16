b24a7ee8ec28d9ebc48680217cfca715c3f88d9b
xogus3492 <wkdxogus3492@gmail.com>
2025-03-16 16:10:49 +0900

    feat: Card에 이미지 데이터 바인딩
    + open API 요청 오류 수정

--- src/pages/index/components/Card.tsx
+++ src/pages/index/components/Card.tsx
@@ -1,13 +1,18 @@
 import styles from './Card.module.scss'
+import { CardDTO } from '../types/card'
 
-function Card() {
+interface Props { // props : 부모 컴포넌트가 자식 컴포넌트에 데이터를 전달할때 사용
+  data: CardDTO
+}
+
+function Card({ data }: Props) {
 const openDialog = () => {
     console.log('함수호출')
 }
 
   return (
     <div className={styles.card} onClick={openDialog}>
-        <img src="" alt="" className={styles.card__image} />
+        <img src={data.urls.small} alt={data.alt_description} className={styles.card__image} />
     </div>
   )
 }
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import CommonHeader from '@/components/common/header/CommonHeader'
 import CommonSearchBar from '@/components/common/searchBar/CommonSearchBar'
 import CommonNav from '@/components/common/navigation/CommonNav'
@@ -5,26 +6,35 @@ import CommonFooter from '@/components/common/footer/CommonFooter'
 import Card from './components/Card'
 import styles from './styles/index.module.scss'
 import axios from 'axios'
-import { useEffect } from 'react'
+import { CardDTO } from './types/card'
 
 function index() {
+    const [imgUrls, setImgUrls] = useState([])
+
     const getData = async () => {
         // 모든 API 호출
-        const API_URL = 'https://unsplash.com/search/photos'
-        const API_KEY = import.meta.env.REACT_APP_API_KEY
+        const API_URL = 'https://api.unsplash.com/photos'
+        const API_KEY = import.meta.env.VITE_API_KEY
         const PER_PAGE = 30
 
         const searchValue = 'Korea'
         const pageValue = 100
 
         try {
-            const res = await axios.get('${API_URL}?query=${searchValue}&client_id=${API_KEY}&page=${pageValue}&per_page=${PER_PAGE}')
-
+            const res = await axios.get(`${API_URL}?query=${searchValue}&client_id=${API_KEY}&page=${pageValue}&per_page=${PER_PAGE}`)
             console.log(res)
+
+            if (res.status === 200) {
+                setImgUrls(res.data)
+            }
         } catch (error) {
             console.log(error)
         }
     }
+
+    const cardList = imgUrls.map((card: CardDTO)=>{
+        return <Card data={card} key={card.id} />
+    })
 
     useEffect(() => {
         getData()
@@ -48,12 +58,7 @@ function index() {
                         <CommonSearchBar />
                     </div>
                 </div>
-                <div className={styles.page__contents__imageBox}>
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                </div>
+                <div className={styles.page__contents__imageBox}>{cardList}</div>
             </div>
             {/* 공통 풋터 UI 부분 */}
             <CommonFooter />
