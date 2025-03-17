8e8eb469de0cc6c9fa3999c87db7f492cac8f454
xogus3492 <wkdxogus3492@gmail.com>
2025-03-17 13:41:04 +0900

    refactor: Card 기능 리팩토링
    recoil 활용

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import MainPage from '@/pages/index/index' // 페이지 컴포넌트
+import { RecoilRoot } from 'recoil'
 
 function App() {
   return (
-    <BrowserRouter>
+    <RecoilRoot>
+      <BrowserRouter>
       <Routes>
         <Route index path="/" element={<MainPage />}></Route>
       </Routes>
     </BrowserRouter>
+    </RecoilRoot>
   )
 }
 
