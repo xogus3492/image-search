93376bd13267413d62ea612e071d87f01f32ebb7
xogus3492 <wkdxogus3492@gmail.com>
2025-03-18 16:42:05 +0900

    feat: 이미지 상세 다이얼로그 컴포넌트 구현

--- src/pages/index/components/Card.tsx
+++ src/pages/index/components/Card.tsx
@@ -3,11 +3,13 @@ import { CardDTO } from '../types/card'
 
 interface Props { // props : 부모 컴포넌트가 자식 컴포넌트에 데이터를 전달할때 사용
   data: CardDTO
+  handleDialog: (eventValue: boolean) => void
 }
 
-function Card({ data }: Props) {
+function Card({ data, handleDialog }: Props) {
 const openDialog = () => {
     console.log('함수호출')
+    handleDialog(true)
 }
 
   return (
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -5,6 +5,7 @@ import CommonHeader from '@/components/common/header/CommonHeader'
 import CommonSearchBar from '@/components/common/searchBar/CommonSearchBar'
 import CommonNav from '@/components/common/navigation/CommonNav'
 import CommonFooter from '@/components/common/footer/CommonFooter'
+import DetailDialog from '@/components/common/dialog/DetailDialog'
 import Card from './components/Card'
 import styles from './styles/index.module.scss'
 import { CardDTO } from './types/card'
@@ -12,9 +13,10 @@ import { CardDTO } from './types/card'
 function index() {
     const imgSelector = useRecoilValue(imageData)
     const [imgData, setImgData] = useState<CardDTO[]>([])
+    const [open, setOpen] = useState<boolean>(false) // 이미지 상세 다이얼로그 발생(관리) State
 
     const CARD_LIST = imgSelector.data.map((card: CardDTO)=>{
-        return <Card data={card} key={card.id} />
+        return <Card data={card} key={card.id} handleDialog={setOpen} />
     })
 
     return (
@@ -39,6 +41,7 @@ function index() {
             </div>
             {/* 공통 풋터 UI 부분 */}
             <CommonFooter />
+            {open && <DetailDialog />}
         </div>
     )
 }
